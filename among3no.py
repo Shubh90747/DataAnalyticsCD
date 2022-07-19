463bcbaf728a119e1a9848402434becb65af42a8
Shubh90747 <107957136+Shubh90747@users.noreply.github.com>
2022-07-18 22:43:20 -0700

    this is a new program for amoung 3 high no.

---
+++ among3no.py
@@ -0,0 +1,16 @@
+from calendar import c
+
+
+class num:
+    def h():
+        if a>c and a>b:
+            return a
+        if c>a and c>b:
+            return c
+        else:
+            return b
+a=int(input("a"))
+b=int(input("b"))
+c=int(input("c"))
+ans=num.h()
+print("the hight no is:-",ans)
